e7bb4af2579b1cb88c68fa882fd5f04164288665
Hoyeoun Byun <jay08111@naver.com>
2022-03-11 00:48:00 +0900

    # FEAT - makin' facebook

---
+++ facebook/data.js
@@ -0,0 +1,18 @@
+export const navCenterData = [
+  {
+    id: 1,
+    icon: <i class="fas fa-camera"></i>,
+  },
+  {
+    id: 2,
+    icon: <i class="fas fa-camera"></i>,
+  },
+  {
+    id: 3,
+    icon: <i class="fas fa-camera"></i>,
+  },
+  {
+    id: 4,
+    icon: <i class="fas fa-camera"></i>,
+  },
+];

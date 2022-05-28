1db140a9ad4f7f157e8cfa7c6ea1074502fc623d
Hoyeoun <jay08111@naver.com>
2022-05-28 17:43:06 +0900

    # PRACTICING

--- d3/app.js
+++ d3/app.js
@@ -1,14 +1,52 @@
-import axios from "axios";
+const button = document.createElement("button");
+document.querySelector("body").appendChild(button);
 const ul = document.createElement("ul");
-for (let i = 0; i < 5; i++) {
-  const li = document.createElement("li");
-  document.querySelector("body").appendChild(ul).appendChild(li);
-}
-const data = [10, 20, 30, 40, 50];
-const el = d3
-  .select("ul")
-  .selectAll("li")
-  .data(data)
-  .join()
-  .text((d) => d);
-console.log(el);
+const makeElement = (data, type) => {
+  if (type === "csv") {
+    for (const element of data) {
+      const li = document.createElement("li");
+      document.querySelector("body").appendChild(ul).appendChild(li);
+    }
+    d3.select("ul")
+      .selectAll("li")
+      .data(data)
+      .join()
+      .text((d) => d);
+  } else {
+    for (const element of data) {
+      const li = document.createElement("li");
+      document.querySelector("body").appendChild(ul).appendChild(li);
+    }
+    d3.select("ul")
+      .selectAll("li")
+      .data(data)
+      .join()
+      .text((d) => d);
+  }
+  for (const element of data) {
+    const li = document.createElement("li");
+    document.querySelector("body").appendChild(ul).appendChild(li);
+  }
+};
+const getData = async (type) => {
+  if (type === "json") {
+    await d3.json("data.json").then((res) => {
+      makeElement(res, type);
+    });
+  } else if (type === "csv") {
+    await d3.csv("data.csv").then((res) => {
+      makeElement(res, type);
+    });
+  }
+};
+
+button.textContent = "Click to fetch";
+let toggleType = false;
+button.addEventListener("click", () => {
+  toggleType = !toggleType;
+  if (toggleType) {
+    getData("json");
+  } else {
+    getData("csv");
+  }
+});
